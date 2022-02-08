c0e0bae33d26400bf6514a0d552f021411ef0c74
Tedo Manvelidze <tedo.manvelidze@space.ge>
2022-02-09 01:37:09 +0400

    add SignIn screen

---
+++ auth/src/main/java/chat/revolt/auth/SignInFragment.kt
@@ -0,0 +1,37 @@
+/*
+ * Created by Tedo Manvelidze(ted3x) on 2/9/22, 1:31 AM
+ * Copyright (c) 2022 . All rights reserved.
+ * Last modified 2/9/22, 1:31 AM
+ */
+
+package chat.revolt.auth
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SignInFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = SignInFragment()
+    }
+
+    private lateinit var viewModel: SignInViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.sign_in_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(SignInViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ auth/src/main/java/chat/revolt/auth/SignInViewModel.kt
@@ -0,0 +1,7 @@
+package chat.revolt.auth
+
+import androidx.lifecycle.ViewModel
+
+class SignInViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
