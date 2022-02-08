863e18dd3194ba6b6a39911e95ba655bd77234a3
Tedo Manvelidze <tedo.manvelidze@space.ge>
2022-02-09 00:58:02 +0400

    remove hcaptcha from MainActivity.kt

--- app/src/main/java/ge/ted3x/revolt/MainActivity.kt
+++ app/src/main/java/ge/ted3x/revolt/MainActivity.kt
@@ -1,24 +1,11 @@
 package ge.ted3x.revolt
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.hcaptcha.sdk.HCaptcha
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.captcha).setOnClickListener { captcha()}
-    }
-
-    private fun captcha() {
-        HCaptcha.getClient(this).verifyWithHCaptcha("3daae85e-09ab-4ff6-9f24-e8f4f335e433")
-            .addOnSuccessListener {
-                print(it.tokenResult)
-            }
-            .addOnFailureListener {
-                print(it)
-            }
     }
 }
