89df5a8bf70c07aa4d6e8e24b8722cd0ac1a3040
Tedo Manvelidze <tedo.manvelidze@space.ge>
2022-02-10 01:13:44 +0400

    add login response/request domain models

---
+++ auth/src/main/java/chat/revolt/auth/domain/models/request/LoginRequest.kt
@@ -0,0 +1,15 @@
+/*
+ * Created by Tedo Manvelidze(ted3x) on 2/9/22, 8:16 PM
+ * Copyright (c) 2022 . All rights reserved.
+ * Last modified 2/9/22, 8:14 PM
+ */
+
+package chat.revolt.auth.domain.models.request
+
+data class LoginRequest(
+    val email: String,
+    val password: String,
+    val challenge: String? = null,
+    val friendlyName: String = "Android",
+    val captcha: String
+)
