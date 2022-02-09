49260fa42b3819aecf4855cee1d7649eff0f10e8
Tedo Manvelidze <tedo.manvelidze@space.ge>
2022-02-09 23:50:42 +0400

    add PasswordStates.kt

---
+++ auth/src/main/java/chat/revolt/auth/states/PasswordStates.kt
@@ -0,0 +1,15 @@
+/*
+ * Created by Tedo Manvelidze(ted3x) on 2/9/22, 10:27 PM
+ * Copyright (c) 2022 . All rights reserved.
+ * Last modified 2/9/22, 10:27 PM
+ */
+
+package chat.revolt.auth.states
+
+sealed class PasswordStates {
+    object Valid: PasswordStates()
+    data class Short(val message: String): PasswordStates()
+    data class Long(val message: String): PasswordStates()
+
+    val isValid get() = this is Valid
+}
