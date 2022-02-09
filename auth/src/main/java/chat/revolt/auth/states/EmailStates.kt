b1c0b4ee0d4968e7345a31da0f08d74c80a170eb
Tedo Manvelidze <tedo.manvelidze@space.ge>
2022-02-09 23:50:35 +0400

    add EmailStates.kt

---
+++ auth/src/main/java/chat/revolt/auth/states/EmailStates.kt
@@ -0,0 +1,14 @@
+/*
+ * Created by Tedo Manvelidze(ted3x) on 2/9/22, 10:17 PM
+ * Copyright (c) 2022 . All rights reserved.
+ * Last modified 2/9/22, 10:17 PM
+ */
+
+package chat.revolt.auth.states
+
+sealed class EmailStates {
+    object Valid: EmailStates()
+    data class Invalid(val message: String): EmailStates()
+
+    val isValid get() = this is Valid
+}
