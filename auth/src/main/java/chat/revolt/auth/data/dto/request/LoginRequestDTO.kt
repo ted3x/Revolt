94bbb5905eba13e320e9a68458b07d4a7c70c282
Tedo Manvelidze <tedo.manvelidze@space.ge>
2022-02-10 01:13:20 +0400

    add retrofit and retrofit-moshi in auth

---
+++ auth/src/main/java/chat/revolt/auth/data/dto/request/LoginRequestDTO.kt
@@ -0,0 +1,18 @@
+/*
+ * Created by Tedo Manvelidze(ted3x) on 2/9/22, 8:16 PM
+ * Copyright (c) 2022 . All rights reserved.
+ * Last modified 2/9/22, 8:14 PM
+ */
+
+package chat.revolt.auth.data.dto.request
+
+import com.squareup.moshi.Json
+
+data class LoginRequestDTO(
+    val email: String,
+    val password: String,
+    val challenge: String? = null,
+    @Json(name = "friendly_name")
+    val friendlyName: String = "Android",
+    val captcha: String
+)
---
+++ auth/src/main/java/chat/revolt/auth/data/dto/response/LoginResponseDTO.kt
@@ -0,0 +1,19 @@
+/*
+ * Created by Tedo Manvelidze(ted3x) on 2/9/22, 8:14 PM
+ * Copyright (c) 2022 . All rights reserved.
+ * Last modified 2/9/22, 8:14 PM
+ */
+
+package chat.revolt.auth.data.dto.response
+
+import com.squareup.moshi.Json
+
+data class LoginResponseDTO(
+    @Json(name = "_id")
+    val id: String?,
+    @Json(name = "user_id")
+    val userId: String,
+    val token: String,
+    val name: String,
+    val subscription: String?
+)
