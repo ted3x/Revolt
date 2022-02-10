71ba0c19cf9f3e9f26ff714d1c3eb6abc7fd8fe2
Tedo Manvelidze <tedo.manvelidze@space.ge>
2022-02-10 14:00:33 +0400

    refactor SignIn screen

--- auth/src/main/java/chat/revolt/auth/SignInFragment.kt
+++ auth/src/main/java/chat/revolt/auth/SignInFragment.kt
@@ -6,14 +6,71 @@
 
 package chat.revolt.auth
 
+import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import chat.revolt.auth.databinding.SignInFragmentBinding
+import chat.revolt.auth.states.EmailStates
+import chat.revolt.auth.states.PasswordStates
+import chat.revolt.core.extensions.onChange
 import chat.revolt.core.fragment.BaseFragment
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import java.lang.ref.WeakReference
 
-class SignInFragment : BaseFragment<SignInViewModel, SignInFragmentBinding>(SignInFragmentBinding::inflate) {
+class SignInFragment :
+    BaseFragment<SignInViewModel, SignInFragmentBinding>(SignInFragmentBinding::inflate) {
 
-    override val viewModel: SignInViewModel
-        get() {
-            TODO()
+    override lateinit var viewModel: SignInViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this)[SignInViewModel::class.java]
+        // TODO: Use the ViewModel
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.signIn.setOnClickListener { viewModel.solveCaptcha(WeakReference(requireActivity())) }
+        setupTextFieldListeners()
+        setupStateListeners()
+        setupObservers()
+    }
+
+    private fun setupTextFieldListeners() {
+        binding.emailEditText.onChange(viewModel::onEmailTextFieldChange)
+        binding.passwordEditText.onChange(viewModel::onPasswordTextFieldChange)
+    }
+
+    private fun setupStateListeners() {
+        lifecycleScope.launchWhenResumed {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                launch { viewModel.emailState.collect { onEmailStateChange(it) } }
+                launch { viewModel.passwordState.collect { onPasswordStateChange(it) } }
+            }
         }
+    }
 
+    private fun onEmailStateChange(emailStates: EmailStates) {
+        when (emailStates) {
+            is EmailStates.Valid -> binding.textField.error = null
+            is EmailStates.Invalid -> binding.textField.error = emailStates.message
+        }
+    }
+
+    private fun onPasswordStateChange(passwordStates: PasswordStates) {
+        when (passwordStates) {
+            is PasswordStates.Valid -> binding.passwordTextField.error = null
+            is PasswordStates.Short -> binding.passwordTextField.error = passwordStates.message
+            is PasswordStates.Long -> binding.passwordTextField.error = passwordStates.message
+        }
+    }
+
+    private fun setupObservers() {
+        viewModel.email.observe { viewModel.validateEmail(it) }
+        viewModel.password.observe { viewModel.validatePassword(it) }
+        viewModel.captcha.observe { viewModel.signIn() }
+    }
 }
--- auth/src/main/java/chat/revolt/auth/SignInViewModel.kt
+++ auth/src/main/java/chat/revolt/auth/SignInViewModel.kt
@@ -6,8 +6,85 @@
 
 package chat.revolt.auth
 
+import android.content.Context
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import chat.revolt.auth.data.captcha_manager.CaptchaManagerImpl
+import chat.revolt.auth.domain.captcha_manager.CaptchaListener
+import chat.revolt.auth.domain.captcha_manager.CaptchaManager
+import chat.revolt.auth.domain.interactor.EmailValidation
+import chat.revolt.auth.domain.interactor.PasswordValidation
+import chat.revolt.auth.states.EmailStates
+import chat.revolt.auth.states.PasswordStates
+import chat.revolt.auth.utils.isValidEmail
+import chat.revolt.auth.utils.isValidPassword
 import chat.revolt.core.view_model.BaseViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+import java.lang.ref.WeakReference
 
-class SignInViewModel : BaseViewModel() {
-    // TODO: Implement the ViewModel
+class SignInViewModel(
+    private val passwordValidation: PasswordValidation,
+    private val emailValidation: EmailValidation,
+    private val captchaManager: CaptchaManager = CaptchaManagerImpl(""),
+) :
+    BaseViewModel() {
+
+    val email = MutableLiveData<String>()
+    val emailState = MutableStateFlow<EmailStates>(EmailStates.Valid)
+    val password = MutableLiveData<String>()
+    val passwordState = MutableStateFlow<PasswordStates>(PasswordStates.Valid)
+    val captcha = MutableLiveData<String>()
+
+    private val captchaListener = object : CaptchaListener {
+        override fun onSuccess(captcha: String) {
+            this@SignInViewModel.captcha.value = captcha
+        }
+
+        override fun onFail(error: String?) {
+            error?.let { snackBarMessage.value = it }
+        }
+    }
+
+    init {
+        captchaManager.setListener(captchaListener)
+    }
+
+    fun solveCaptcha(ctx: WeakReference<Context>) {
+        if (email.value.isValidEmail() && password.value.isValidPassword())
+            captchaManager.solveCaptcha(ctx)
+        else {
+            validateEmail(email.value)
+            validatePassword(password.value)
+        }
+    }
+
+    fun signIn() {
+        TODO()
+    }
+
+    fun onEmailTextFieldChange(email: String) {
+        this@SignInViewModel.email.value = email
+    }
+
+    fun onPasswordTextFieldChange(password: String) {
+        this@SignInViewModel.password.value = password
+    }
+
+    fun validateEmail(email: String?) {
+        viewModelScope.launch {
+            emailState.emit(emailValidation.invoke(email))
+        }
+    }
+
+    fun validatePassword(password: String?) {
+        viewModelScope.launch {
+            passwordState.emit(passwordValidation.invoke(password))
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        captchaManager.removeListener()
+    }
 }
