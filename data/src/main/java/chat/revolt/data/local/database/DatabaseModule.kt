1c6ed34c4d77dec53e0957d20e6e3c691e12a5ac
Tedo Manvelidze <tedo.manvelidze@space.ge>
2022-02-11 01:26:58 +0400

    remove comment of userDao definition

--- data/src/main/java/chat/revolt/data/local/database/DatabaseModule.kt
+++ data/src/main/java/chat/revolt/data/local/database/DatabaseModule.kt
@@ -1,15 +1,15 @@
 /*
- * Created by Tedo Manvelidze(ted3x) on 2/10/22, 4:51 PM
+ * Created by Tedo Manvelidze(ted3x) on 2/10/22, 8:41 PM
  * Copyright (c) 2022 . All rights reserved.
- * Last modified 2/10/22, 4:51 PM
+ * Last modified 2/10/22, 8:22 PM
  */
 
-package chat.revolt.data.database
+package chat.revolt.data.local.database
 
 import androidx.room.Room
 import org.koin.dsl.module
 
 val databaseModule = module {
     single { Room.databaseBuilder(get(), RevoltDatabase::class.java, "Revolt-Database").build()}
-    //single { get<RevoltDatabase>().userDao() }
+    single { get<RevoltDatabase>().userDao() }
 }
