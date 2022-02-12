dad7aa6f4f6cc3eb89ae39a0ff8a90266a8df494
Tedo Manvelidze <tedo.manvelidze@space.ge>
2022-02-12 19:10:13 +0400

    make MainActivity extend BaseActivity

--- app/src/main/java/chat/revolt/app/MainActivity.kt
+++ app/src/main/java/chat/revolt/app/MainActivity.kt
@@ -7,17 +7,19 @@
 package chat.revolt.app
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import android.view.ViewStub
+import android.widget.FrameLayout
+import chat.revolt.core.BaseActivity
 import chat.revolt.core_navigation.features.Feature
 import chat.revolt.core_navigation.features.auth.AuthStates
 import chat.revolt.core_navigation.navigator.GlobalNavigator
 import com.github.terrakok.cicerone.NavigatorHolder
 import com.github.terrakok.cicerone.androidx.AppNavigator
 import org.koin.android.ext.android.inject
-import org.koin.androidx.scope.ScopeActivity
 import org.koin.core.parameter.parametersOf
 
-class MainActivity : ScopeActivity() {
+class MainActivity : BaseActivity() {
     private val navigatorHolder by inject<NavigatorHolder>()
     private val appNavigator by inject<AppNavigator> { parametersOf(this, R.id.container) }
     private val globalNavigator by inject<GlobalNavigator>()
@@ -36,5 +38,19 @@ class MainActivity : ScopeActivity() {
     override fun onPause() {
         navigatorHolder.removeNavigator()
         super.onPause()
+    }
+
+   override fun showLoading() {
+        val loadingView = findViewById<ViewStub>(R.id.loadingViewStub).inflate()
+        val container = findViewById<FrameLayout>(R.id.container)
+        container.alpha = 0.8f
+        loadingView.visibility = View.VISIBLE
+    }
+
+    override fun hideLoading() {
+        val loadingView = findViewById<FrameLayout>(R.id.loadingView)
+        val container = findViewById<FrameLayout>(R.id.container)
+        container.alpha = 1f
+        loadingView.visibility = View.GONE
     }
 }
