063cb5a917e96f28a66fee4772587b6f8d8c68de
Tedo Manvelidze <tedo.manvelidze@space.ge>
2022-02-12 18:20:21 +0400

    add UseCaseExtensions.kt

---
+++ core/src/main/java/chat/revolt/core/extensions/UseCaseExtensions.kt
@@ -0,0 +1,30 @@
+/*
+ * Created by Tedo Manvelidze(ted3x) on 2/12/22, 6:16 PM
+ * Copyright (c) 2022 . All rights reserved.
+ * Last modified 2/12/22, 6:16 PM
+ */
+
+package chat.revolt.core.extensions
+
+import chat.revolt.core.NetworkErrorHandler
+import chat.revolt.domain.interactors.BaseUseCase
+import org.koin.java.KoinJavaComponent.get
+
+suspend fun <I, O> BaseUseCase<I, O>.execute(
+    errorHandler: NetworkErrorHandler = get(NetworkErrorHandler::class.java),
+    params: I,
+    onLoading: ((isLoading: Boolean) -> Unit)? = null,
+    onSuccess: (O) -> Unit,
+    onError: ((Throwable) -> Unit)? = null
+) {
+    onLoading?.invoke(true)
+    try {
+        onSuccess.invoke(this.invoke(params))
+    }
+    catch (throwable: Throwable) {
+        onError?.invoke(throwable) ?: errorHandler.handleException(throwable)
+    }
+    finally {
+        onLoading?.invoke(false)
+    }
+}
