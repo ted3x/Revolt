1de13936d8552323b555708a371bdf2a592db414
Tedo Manvelidze <tedo.manvelidze@space.ge>
2022-02-12 23:54:43 +0400

    add fetchRevoltConfig in Splash
    Signed-off-by: Tedo Manvelidze <tedo.manvelidze@space.ge>

--- splash/src/main/java/chat/revolt/splash/presentation/ui/SplashFragment.kt
+++ splash/src/main/java/chat/revolt/splash/presentation/ui/SplashFragment.kt
@@ -10,18 +10,17 @@ import android.os.Bundle
 import android.view.View
 import chat.revolt.core.fragment.BaseFragment
 import chat.revolt.splash.databinding.SplashFragmentBinding
+import chat.revolt.splash.presentation.di.splashModules
 import chat.revolt.splash.presentation.vm.SplashViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import org.koin.core.module.Module
 
 class SplashFragment :
     BaseFragment<SplashViewModel, SplashFragmentBinding>(SplashFragmentBinding::inflate) {
     override val viewModel: SplashViewModel by viewModel()
-    override val module: List<Module>
-        get() = TODO("Not yet implemented")
+    override val module = splashModules
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel.getServerConfig()
+        viewModel.fetchRevoltConfig()
     }
 }
--- splash/src/main/java/chat/revolt/splash/presentation/vm/SplashViewModel.kt
+++ splash/src/main/java/chat/revolt/splash/presentation/vm/SplashViewModel.kt
@@ -6,12 +6,30 @@
 
 package chat.revolt.splash.presentation.vm
 
+import androidx.lifecycle.viewModelScope
+import chat.revolt.core.extensions.execute
+import chat.revolt.core.server_config.RevoltConfigManager
 import chat.revolt.core.view_model.BaseViewModel
+import chat.revolt.core_navigation.features.Feature
+import chat.revolt.core_navigation.features.auth.AuthStates
 import chat.revolt.core_navigation.navigator.GlobalNavigator
+import chat.revolt.splash.domain.interactors.GetRevoltConfigUseCase
+import kotlinx.coroutines.launch
 
-class SplashViewModel(private val globalNavigator: GlobalNavigator): BaseViewModel() {
+class SplashViewModel(
+    private val globalNavigator: GlobalNavigator,
+    private val getRevoltConfigUseCase: GetRevoltConfigUseCase,
+    private val revoltConfigManager: RevoltConfigManager
+) : BaseViewModel() {
 
-    fun getServerConfig() {
-
+    fun fetchRevoltConfig() {
+        viewModelScope.launch {
+            getRevoltConfigUseCase.execute(params = Unit,
+                onSuccess = { revoltConfig ->
+                    revoltConfigManager.setConfig(revoltConfig)
+                    globalNavigator.navigateTo(Feature.Auth(state = AuthStates.SignIn))
+                }
+            )
+        }
     }
 }
