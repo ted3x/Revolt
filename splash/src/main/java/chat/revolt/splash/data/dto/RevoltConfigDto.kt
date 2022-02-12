2cf6d75898edb11977f8bfb9b45cdf7f79dd5626
Tedo Manvelidze <tedo.manvelidze@space.ge>
2022-02-12 23:45:44 +0400

    add RevoltConfigDto.kt
    Signed-off-by: Tedo Manvelidze <tedo.manvelidze@space.ge>

--- splash/src/main/java/chat/revolt/splash/data/dto/RevoltConfigDto.kt
+++ splash/src/main/java/chat/revolt/splash/data/dto/RevoltConfigDto.kt
@@ -1,46 +1,46 @@
 /*
- * Created by Tedo Manvelidze(ted3x) on 2/12/22, 8:57 PM
+ * Created by Tedo Manvelidze(ted3x) on 2/12/22, 11:35 PM
  * Copyright (c) 2022 . All rights reserved.
- * Last modified 2/12/22, 8:57 PM
+ * Last modified 2/12/22, 11:35 PM
  */
 
-package chat.revolt.core.server_config
+package chat.revolt.splash.data.dto
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-data class RevoltConfig(
+data class RevoltConfigDto(
     @Json(name = "revolt")
     val version: String,
-    val features: Features,
+    val features: FeaturesDto,
     val ws: String,
     val app: String,
     val vapid: String,
 ) {
     @JsonClass(generateAdapter = true)
-    data class Features(
-        val captcha: Captcha,
+    data class FeaturesDto(
+        val captcha: CaptchaDto,
         @Json(name = "email")
         val isEmailVerificationEnabled: Boolean,
         @Json(name = "invite_only")
         val inviteOnly: Boolean,
-        val autumn: FileUploadServer,
-        val january: FileUploadServer,
-        val voso: VoiceServerOptions
+        val autumn: FileUploadServerDto,
+        val january: FileUploadServerDto,
+        val voso: VoiceServerOptionsDto
     ) {
-        data class Captcha(
+        data class CaptchaDto(
             val enabled: Boolean,
             @Json(name = "key")
             val siteKey: String
         )
 
-        data class FileUploadServer(
+        data class FileUploadServerDto(
             val enabled: Boolean,
             val url: String
         )
 
-        data class VoiceServerOptions(
+        data class VoiceServerOptionsDto(
             val enabled: Boolean,
             val url: String,
             val ws: String
